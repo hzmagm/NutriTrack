13bf8666f8f5565e1845d8a4ece46d4c4d3765ec
hzmagm <59263934+hzmagm@users.noreply.github.com>
2023-06-10 19:44:44 +0200

    adding calaculate function and adjust the picker

--- HealthGoals.js
+++ HealthGoals.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import React from 'react';
 import { View, Text } from 'react-native';
-import { TextInput,Pressable,StyleSheet  } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import { TextInput,Pressable,StyleSheet,ScrollView,Alert   } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown'
+
+const activityLevelArray = ["sedentary", "light exercise", "heavy exercise"," moderate exercise", "extra active"]
+const healthGoalArray = ["weight loss", "weight maintenance", "weight gain"]
+const genderArray=["Male", "Female"]
 
 const HealthGoals = () => {
   const [age,setAge]=useState('');
@@ -11,78 +15,116 @@ const HealthGoals = () => {
   const [weight,setWeight]=useState('');
   const [activityLevel,setActivityLevel]=useState('');
   const [healthGoal,setHealthGoal]=useState('');
-  const [isValidtedForm,setIsValidatedForm] = useState(false);
 
   const handleAgeChange= (text)=>{
     setAge(text);
-    validateEntries();
-  }
-
-  const handleGenderChange= (text)=>{
-    setGender(text);
-    validateEntries();
   }
 
   const handleHeightChange= (text)=>{
-    setHeight(text);
-    validateEntries();
+    const formattedText = text.replace(/,/g, ".");
+    const parsedHeight = parseFloat(formattedText);
+    setHeight(parsedHeight);
   }
 
   const handleWeightChange= (text)=>{
-    setWeight(text);
-    validateEntries();
+    const formattedText = text.replace(/,/g, ".");
+    const parsedWeight = parseFloat(formattedText);
+    setWeight(parsedWeight);
   }
 
   const handleActivityLevelChange= (text)=>{
     setActivityLevel(text);
-    validateEntries();
   }
 
   const handleHealthGoalChange= (text)=>{
     setHealthGoal(text);
-    validateEntries();
+  }
+
+  const handleGenderChange= (text)=>{
+    setGender(text);
   }
 
   const handleCalculate = () =>{
-    console.log(isValidtedForm);
+    BMR=0;
+    if(!(age && height && weight && gender && activityLevel && healthGoal)){
+      Alert.alert('Validation Error', 'Please fill in all the fields.');
+    }else{
+      if(gender=="Male"){
+        BMR = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age ;
+      }else if(gender=="Female"){
+        BMR = 447.593 + 9.247 * weight + 3.098 * height - 4.330 * age ;
+     }
+     if(activityLevel=="sedentary"){
+      BMR=BMR*1.2;
+     }else if(activityLevel=="light exercise"){
+      BMR=BMR*1.375;
+     }else if(activityLevel=="moderate exercise"){
+      BMR=BMR*1.55;
+     }else if(activityLevel=="heavy exercise"){
+      BMR=BMR*1.725;
+     }else if(activityLevel=="extra active"){
+      BMR=BMR*1.9;
+     }
+     if(healthGoal=="weight loss"){
+        BMR = BMR-BMR * 0.1 ;
+     }else if(healthGoal=="weight gain"){
+        BMR = BMR+BMR * 0.1 ;
+     }
+     Alert.alert('Validation Error', BMR);
+    }
   }
-  const validateEntries = () =>{
-    if(age && gender && height && weight && activityLevel && healthGoal){
-        setIsValidatedForm(true);
-    }
-    else{
-      setIsValidatedForm(false);
-    }
-}
+
   return (
+<ScrollView keyboardShouldPersistTaps='handled' scrollEnabled={false}>
       <View style={styles.container}>
-        <View style={styles.form}>
-          <Text style={styles.InputName}>Age </Text>
-          <TextInput style={styles.input} value={age} onChangeText={handleAgeChange}></TextInput>
-          <Text style={styles.InputName}>Gender </Text>
-          <TextInput style={styles.input} value={gender} onChangeText={handleGenderChange}></TextInput>
-          <Text style={styles.InputName}>Height </Text>
-          <TextInput style={styles.input} value={height} onChangeText={handleHeightChange}></TextInput>
-          <Text style={styles.InputName}>Weight </Text>
-          <TextInput style={styles.input} value={weight} onChangeText={handleWeightChange}></TextInput>
-          <Text style={styles.InputName}>Activity level </Text>
-          <Picker style={styles.input} selectedValue={activityLevel} onValueChange={handleActivityLevelChange}>
-                  <Picker.Item label="sedentary" value="light" />
-                  <Picker.Item label="light exercise" value="moderate" />
-                  <Picker.Item label="heavy exercise" value="heavy" />
-                  <Picker.Item label="extra active" value="extra" />
-          </Picker>
-          <Text style={styles.InputName}>Health goal </Text>
-          <TextInput style={styles.input} value={healthGoal} onChangeText={handleHealthGoalChange}></TextInput>
-        </View>
-        <View style={styles.form}>
-          <Pressable style={styles.button} onPress={handleCalculate} disabled={!isValidtedForm}>
-            <Text style={styles.ButtonText}> calculate </Text>
-          </Pressable>
-        </View>
-    </View>
+            <View style={styles.form}>
+                <Text style={styles.InputName} >Age </Text>
+                <TextInput style={styles.input} onChangeText ={handleAgeChange} inputMode="numeric"></TextInput>
+                <Text style={styles.InputName}>Height </Text>
+                <TextInput style={styles.input} onChangeText ={handleHeightChange} keyboardType="decimal-pad"></TextInput>
+                <Text style={styles.InputName}>Weight </Text>
+                <TextInput style={styles.input} onChangeText ={handleWeightChange} keyboardType="decimal-pad"></TextInput>
+                <Text style={styles.InputName}>Gender </Text>
+                <SelectDropdown
+                  data={genderArray}
+                  onSelect={(selectedItem) => {
+                    handleGenderChange(selectedItem);
+                  }}
+                  buttonTextAfterSelection={(selectedItem) => {
+                    return selectedItem
+                  }}
+                />
+                <Text style={styles.InputName}>Health goal </Text>
+                <SelectDropdown
+                  data={healthGoalArray}
+                  onSelect={(selectedItem) => {
+                    handleHealthGoalChange(selectedItem);
+                  }}
+                  buttonTextAfterSelection={(selectedItem) => {
+                    return selectedItem
+                  }}
+                />
+                <Text style={styles.InputName}>Activity level</Text>
+                <SelectDropdown
+                  data={activityLevelArray}
+                  onSelect={(selectedItem) => {
+                    handleActivityLevelChange(selectedItem);
+                  }}
+                  buttonTextAfterSelection={(selectedItem) => {
+                    return selectedItem
+                  }}
+                />
+            </View>
+          <View style={styles.form}>
+            <Pressable style={styles.button} onPress={handleCalculate} >
+              <Text style={styles.ButtonText}> calculate </Text>
+            </Pressable>
+          </View>
+      </View>
+      </ScrollView>
   );
 };
+
 const styles = StyleSheet.create({
   container:{
     flex: 1,
@@ -99,6 +141,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginBottom: 10,
   },
+  inputPicker:{
+    width: 200,
+    height: 50,
+    borderColor: 'red',
+    marginBottom: 100,
+  },
   button: {
     width: 150,
     height: 40,
@@ -112,7 +160,6 @@ const styles = StyleSheet.create({
     lineHeight: 21,
     fontWeight: 'bold',
     color: 'white',
-    backgroundColor: 'red',
   },
   form : {
     paddingTop:30,
